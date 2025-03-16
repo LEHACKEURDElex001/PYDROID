c92d262b90646a2ee35e0a1f739f865b9e5eb354
P1p1t0t0 <149186990+LEHACKEURDElex001@users.noreply.github.com>
2025-03-16 04:54:51 +0100

    Add files via upload

---
+++ Logo-virus.py
@@ -0,0 +1,20 @@
+import turtle
+
+a = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor('black')
+a.speed(0)
+a.hideturtle()
+
+a.color('cyan')
+
+for x in range(200):
+    a.forward(x)
+    a.left(x - 1)
+
+a.penup()
+a.goto(-200, 150)
+a.pendown()
+
+a.color('cyan')
+a.write
